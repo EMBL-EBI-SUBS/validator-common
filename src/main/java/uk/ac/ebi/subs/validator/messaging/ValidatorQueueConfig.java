56e4c6d35a8a7a002bdaf86d10d64b6685a228d0
Flavia Penim <flaviapenim@gmail.com>
2017-09-12 14:38:11 +0100

    Removing empty configuration file.

--- src/main/java/uk/ac/ebi/subs/validator/messaging/ValidatorQueueConfig.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.ebi.subs.validator.messaging;
-
-/**
- * This class is responsible for the configuration of the validation queues.
- *
- * Created by karoly on 28/04/2017.
- */
-public class ValidatorQueueConfig {}
