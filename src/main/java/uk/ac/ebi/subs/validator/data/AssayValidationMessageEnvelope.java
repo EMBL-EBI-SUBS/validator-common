22267c7998570fe1cde36173eae3ec60bd93f07e
Neil Goodgame <neilg@ebi.ac.uk>
2018-01-29 15:22:49 +0000

    Added the referenceable submittables to the ValidationMessageEnvelope

--- src/main/java/uk/ac/ebi/subs/validator/data/AssayValidationMessageEnvelope.java
+++ src/main/java/uk/ac/ebi/subs/validator/data/AssayValidationMessageEnvelope.java
@@ -1,6 +1,11 @@
 package uk.ac.ebi.subs.validator.data;
 
 import uk.ac.ebi.subs.data.submittable.Assay;
+import uk.ac.ebi.subs.data.submittable.Sample;
+import uk.ac.ebi.subs.data.submittable.Study;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This is a Data Transfer Object transferring {@link Assay} data from the {@code validator-coordinator} service
@@ -10,5 +15,22 @@ import uk.ac.ebi.subs.data.submittable.Assay;
  * Created by karoly on 06/10/2017.
  */
 public class AssayValidationMessageEnvelope extends ValidationMessageEnvelope<Assay> {
+    private Study study;
+    private List<Sample> sampleList = new ArrayList();
 
+    public Study getStudy() {
+        return study;
+    }
+
+    public void setStudy(Study study) {
+        this.study = study;
+    }
+
+    public List<Sample> getSampleList() {
+        return sampleList;
+    }
+
+    public void setSampleList(List<Sample> sampleList) {
+        this.sampleList = sampleList;
+    }
 }
