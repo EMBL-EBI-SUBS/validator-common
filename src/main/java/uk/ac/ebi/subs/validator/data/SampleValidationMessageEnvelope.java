ee707642cd53ad2ffae8b70ae3a8a26988899983
Neil Goodgame <neilg@ebi.ac.uk>
2018-02-02 13:26:21 +0000

    Added sampleList to enable the validation of sample relationships

--- src/main/java/uk/ac/ebi/subs/validator/data/SampleValidationMessageEnvelope.java
+++ src/main/java/uk/ac/ebi/subs/validator/data/SampleValidationMessageEnvelope.java
@@ -1,6 +1,10 @@
 package uk.ac.ebi.subs.validator.data;
 
 import uk.ac.ebi.subs.data.submittable.Sample;
+import uk.ac.ebi.subs.validator.model.Submittable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This is a Data Transfer Object transferring {@link Sample} data from the {@code validator-coordinator} service
@@ -8,8 +12,12 @@ import uk.ac.ebi.subs.data.submittable.Sample;
  * A container object holding the UUID (MongoDB ID) and the {@link Sample} entity to validate.
  *
  * Created by karoly on 06/10/2017.
+ *
+ * Sample may contain relationships that need to be included in the ValidationMessageEnvelope
  */
 public class SampleValidationMessageEnvelope extends ValidationMessageEnvelope<Sample> {
+    private List<Submittable<Sample>> sampleList = new ArrayList();
+
     public SampleValidationMessageEnvelope() {
     }
 
@@ -18,5 +26,13 @@ public class SampleValidationMessageEnvelope extends ValidationMessageEnvelope<S
                                            Sample entityToValidate,
                                            String submissionId) {
         super(validationResultUUID, validationResultVersion, entityToValidate, submissionId);
+    }
+
+    public List<Submittable<Sample>> getSampleList() {
+        return sampleList;
+    }
+
+    public void setSampleList(List<Submittable<Sample>> sampleList) {
+        this.sampleList = sampleList;
     }
 }
