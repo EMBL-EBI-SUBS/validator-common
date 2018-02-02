800a4e7daf3d46de33277e5f1f36fb047cf23676
Neil Goodgame <neilg@ebi.ac.uk>
2018-02-02 09:33:15 +0000

    Extends BaseSubmittable by delegation

--- src/main/java/uk/ac/ebi/subs/validator/model/Submittable.java
+++ src/main/java/uk/ac/ebi/subs/validator/model/Submittable.java
@@ -1,8 +1,15 @@
 package uk.ac.ebi.subs.validator.model;
 
+import uk.ac.ebi.subs.data.component.Attribute;
+import uk.ac.ebi.subs.data.component.Attributes;
+import uk.ac.ebi.subs.data.component.Team;
 import uk.ac.ebi.subs.data.submittable.BaseSubmittable;
 
-public class Submittable<T extends BaseSubmittable> {
+
+import java.util.Collection;
+import java.util.Map;
+
+public class Submittable<T extends BaseSubmittable> implements uk.ac.ebi.subs.data.submittable.Submittable, Attributes {
     T baseSubmittable;
     String submissionId;
 
@@ -13,7 +20,6 @@ public class Submittable<T extends BaseSubmittable> {
 
     public Submittable() {
     }
-
 
     public String getSubmissionId() {
         return submissionId;
@@ -29,5 +35,85 @@ public class Submittable<T extends BaseSubmittable> {
 
     public void setBaseSubmittable(T baseSubmittable) {
         this.baseSubmittable = baseSubmittable;
+    }
+
+    @Override
+    public String getId() {
+        return baseSubmittable.getId();
+    }
+
+    @Override
+    public void setId(String id) {
+        baseSubmittable.setId(id);
+    }
+
+    @Override
+    public String getAccession() {
+        return baseSubmittable.getAccession();
+    }
+
+    @Override
+    public void setAccession(String accession) {
+        baseSubmittable.setAccession(accession);
+    }
+
+    @Override
+    public String getAlias() {
+        return baseSubmittable.getAlias();
+    }
+
+    @Override
+    public void setAlias(String alias) {
+        baseSubmittable.setAlias(alias);
+    }
+
+    @Override
+    public Team getTeam() {
+        return baseSubmittable.getTeam();
+    }
+
+    @Override
+    public void setTeam(Team team) {
+        baseSubmittable.setTeam(team);
+    }
+
+    @Override
+    public String getTitle() {
+        return baseSubmittable.getTitle();
+    }
+
+    @Override
+    public void setTitle(String title) {
+        baseSubmittable.setTitle(title);
+    }
+
+    @Override
+    public String getDescription() {
+        return baseSubmittable.getDescription();
+    }
+
+    @Override
+    public void setDescription(String description) {
+        baseSubmittable.setDescription(description);
+    }
+
+    @Override
+    public Map<String, Collection<Attribute>> getAttributes() {
+        return baseSubmittable.getAttributes();
+    }
+
+    @Override
+    public void setAttributes(Map<String, Collection<Attribute>> attributes) {
+        baseSubmittable.setAttributes(attributes);
+    }
+
+    @Override
+    public boolean isAccessioned() {
+        return baseSubmittable.isAccessioned();
+    }
+
+    @Override
+    public void addAttribute(String name, Attribute attribute) {
+        baseSubmittable.addAttribute(name,attribute);
     }
 }
