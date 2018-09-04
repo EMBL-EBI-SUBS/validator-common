3cdf02dccf6011280d802d36906276255e76b154
David Richardson <davidr@ebi.ac.uk>
2018-09-04 09:39:18 +0100

    include data type and checklist info in the validation message

--- src/main/java/uk/ac/ebi/subs/validator/data/ValidationMessageEnvelope.java
+++ src/main/java/uk/ac/ebi/subs/validator/data/ValidationMessageEnvelope.java
@@ -3,17 +3,13 @@ package uk.ac.ebi.subs.validator.data;
 import uk.ac.ebi.subs.data.submittable.BaseSubmittable;
 import uk.ac.ebi.subs.data.submittable.Submittable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.stream.Stream;
 
 /**
  * This is a Data Transfer Object transferring data from the {@code validator-coordinator} service
  * to a specific {@code Validator}.
  * A container object holding the UUID (MongoDB ID) and the entity to validate.
- *
+ * <p>
  * Created by karoly on 08/05/2017.
  */
 public class ValidationMessageEnvelope<T extends BaseSubmittable> {
@@ -22,6 +18,8 @@ public class ValidationMessageEnvelope<T extends BaseSubmittable> {
     private int validationResultVersion;
     private T entityToValidate;
     private String submissionId;
+    private String dataTypeId;
+    private String checklistId;
 
     public ValidationMessageEnvelope() {
     }
@@ -37,6 +35,15 @@ public class ValidationMessageEnvelope<T extends BaseSubmittable> {
         this.validationResultVersion = validationResultVersion;
         this.entityToValidate = entityToValidate;
         this.submissionId = submissionId;
+    }
+
+    public ValidationMessageEnvelope(String validationResultUUID, int validationResultVersion, T entityToValidate, String submissionId, String dataTypeId, String checklistId) {
+        this.validationResultUUID = validationResultUUID;
+        this.validationResultVersion = validationResultVersion;
+        this.entityToValidate = entityToValidate;
+        this.submissionId = submissionId;
+        this.dataTypeId = dataTypeId;
+        this.checklistId = checklistId;
     }
 
     public String getValidationResultUUID() {
@@ -71,8 +78,24 @@ public class ValidationMessageEnvelope<T extends BaseSubmittable> {
         this.submissionId = submissionId;
     }
 
+    public String getDataTypeId() {
+        return dataTypeId;
+    }
+
+    public void setDataTypeId(String dataTypeId) {
+        this.dataTypeId = dataTypeId;
+    }
+
+    public String getChecklistId() {
+        return checklistId;
+    }
+
+    public void setChecklistId(String checklistId) {
+        this.checklistId = checklistId;
+    }
+
     public Stream<uk.ac.ebi.subs.validator.model.Submittable> allSubmissionItemsStream() {
-        uk.ac.ebi.subs.validator.model.Submittable submittable = new uk.ac.ebi.subs.validator.model.Submittable<T>(entityToValidate,submissionId);
+        uk.ac.ebi.subs.validator.model.Submittable submittable = new uk.ac.ebi.subs.validator.model.Submittable<T>(entityToValidate, submissionId);
         return Stream.of(submittable);
     }
 
