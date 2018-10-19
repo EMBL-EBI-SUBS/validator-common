0c5203c2c4a11fcc48897a3801160e0e797f6016
David Richardson <davidrichardson@users.noreply.github.com>
2018-10-19 09:20:51 +0100

    Fix typo
    Co-Authored-By: ke4 <karoly@ebi.ac.uk>

--- src/main/java/uk/ac/ebi/subs/validator/repository/ValidatorResultRepositoryCustom.java
+++ src/main/java/uk/ac/ebi/subs/validator/repository/ValidatorResultRepositoryCustom.java
@@ -46,6 +46,6 @@ public class ValidatorResultRepositoryCustom {
                 )
         );
 
-        return validationIssuesByDattaTypeID;
+        return validationIssuesByDataTypeID;
     }
 }
